92af1cf02f054023e0d50b00e031cc9cd5703c96
Zul Karami <31085513+zulkarom@users.noreply.github.com>
2020-10-14 21:01:37 +0800

    Create BookOrderSearch.php

---
+++ ebook/models/BookOrderSearch.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace ebook\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+
+/**
+ * OrderSearch represents the model behind the search form of `backend\models\DemoToyyib`.
+ */
+class BookOrderSearch extends BookOrder
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'order_id', 'return_status'], 'integer'],
+            [['billcode', 'billName', 'billDescription', 'billTo', 'billEmail', 'billPhone', 'return_response', 'callback_response'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = BookOrder::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'order_id' => $this->order_id,
+            'return_status' => $this->return_status,
+        ]);
+
+        $query->andFilterWhere(['like', 'billcode', $this->billcode])
+            
+            ->andFilterWhere(['like', 'callback_response', $this->callback_response]);
+
+        return $dataProvider;
+    }
+}
