c2c553580d9b7d35aa71f1c209a97de158512326
Zul Karami <31085513+zulkarom@users.noreply.github.com>
2020-10-14 21:56:33 +0800

    Update report.php

--- ebook/views/site/report.php
+++ ebook/views/site/report.php
@@ -7,50 +7,31 @@ use yii\grid\GridView;
 /* @var $searchModel backend\models\DemoToyyibSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Demo Toyyibs';
+$this->title = 'List of buyers';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="demo-toyyib-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <p>
-        <?= Html::a('Create Demo Toyyib', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+  
+    
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-            'order_id',
-            'billName',
-            //'billDescription',
+            'transaction_id',
             'billTo',
-			'return_status',
-            //'billEmail:email',
+'student_id',
+            'billEmail:email',
             'billPhone',
+'group_name',
             //'return_response:ntext',
             //'callback_response:ntext',
 
  
 			
-			['class' => 'yii\grid\ActionColumn',
-                 'contentOptions' => ['style' => 'width: 20%'],
-                'template' => '{update} {pay}',
-                //'visible' => false,
-                'buttons'=>[
-                    'update'=>function ($url, $model) {
-                        return Html::a('<span class="glyphicon glyphicon-pencil"></span> UPDATE',['demo-toyyib/update/', 'id' => $model->id],['class'=>'btn btn-warning btn-sm']);
-                    },
-					
-                    'pay'=>function ($url, $model) {
-                        return Html::a('<span class="glyphicon glyphicon-dollar"></span> PAY',['demo-toyyib/create-bill-page/', 'id' => $model->id],['class'=>'btn btn-warning btn-sm']);
-                    },
-					
-					
-                ],
+			
 				
 				
             
