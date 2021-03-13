257e0ace88e728ce516aaafe06874fbff174ab89
zulkarom <zulkarom@gmail.com>
2021-03-14 07:11:14 +0800

    UPDATE 202020212

--- ebook/models/Book.php
+++ ebook/models/Book.php
@@ -57,12 +57,9 @@ class Book extends \yii\db\ActiveRecord
 	public function groupList(){
 		return [
 			'L1' => 'L1 - DR. AINON @ JAMILAH BINTI RAMLI',
-			'L2' => 'L2 - DR. NOORUL AZWIN BINTI MD NASIR',
+			'L2' => 'L2 - DR. AINON @ JAMILAH BINTI RAMLI',
 			'L3' => 'L3 - EN. ZUL KARAMI BIN CHE MUSA',
-			'L4' => 'L4 - DR. AINON @ JAMILAH BINTI RAMLI',
-			'K1' => 'K1 - DR. NADZIRAH BT MOHD SAID',
-			'L1.FHPK' => 'L1.FHPK - DR. NORMAIZATUL AKMA SAIDI',
-			'L2.FHPK' => 'L2.FHPK - DR. NORMAIZATUL AKMA SAIDI',
+			'L4' => 'L4 - DR. TAHIRAH BINTI ABDULLAH',
 			
 		];
 	}
